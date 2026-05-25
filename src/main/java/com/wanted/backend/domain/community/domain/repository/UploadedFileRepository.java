bbcd1e0ea2c5209b5c8e619ed1d55a1d55ed8d9b
GANGHYEON02 <masan8259@gmail.com>
2026-05-25 23:27:18 +0900

    게시글 삭제 및 수정 완료

--- src/main/java/com/wanted/backend/domain/community/domain/repository/UploadedFileRepository.java
+++ src/main/java/com/wanted/backend/domain/community/domain/repository/UploadedFileRepository.java
@@ -3,5 +3,6 @@ package com.wanted.backend.domain.community.domain.repository;
 import com.wanted.backend.domain.community.domain.model.UploadedFile;
 
 public interface UploadedFileRepository {
+
     UploadedFile save(UploadedFile uploadedFile);
 }
