e9a024e3373ac8b682b3c3c342a8db1573743db8
GANGHYEON02 <masan8259@gmail.com>
2026-05-30 01:51:14 +0900

    커뮤니티 사진 업로드

--- src/main/java/com/wanted/backend/global/config/WebConfig.java
+++ src/main/java/com/wanted/backend/global/config/WebConfig.java
@@ -10,20 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        // 업로드된 파일 (썸네일, 커뮤니티 이미지, 프로필 등)
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:./uploads/");
-
-        //게시글 이미지
-        registry.addResourceHandler("/community/post/**")
+        registry.addResourceHandler("/uploads/community/post/**")
                 .addResourceLocations("file:C:/lecture/Hard-Click-B/Hard-Click-BackEnd/src/main/resources/static/community/post/");
 
-        //댓글 이미지
-        registry.addResourceHandler("/community/comment/**")
+        registry.addResourceHandler("/uploads/community/comment/**")
                 .addResourceLocations("file:C:/lecture/Hard-Click-B/Hard-Click-BackEnd/src/main/resources/static/community/comment/");
-
-        // 프로필 이미지
-        registry.addResourceHandler("/identity/profile/**")
-                .addResourceLocations("file:src/main/resources/static/identity/profile/");
     }
 }
