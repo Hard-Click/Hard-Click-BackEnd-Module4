abb5b4179e2c333d21f5b33c3e9c6f647e2b4800
Yoonjongho1122 <k6k2dao112233@gmail.com>
2026-06-26 16:49:44 +0900

    인덱스 최적화 최종

--- src/main/java/com/wanted/backend/domain/community/infrastructure/persistence/CommentJpaEntity.java
+++ src/main/java/com/wanted/backend/domain/community/infrastructure/persistence/CommentJpaEntity.java
@@ -16,7 +16,10 @@ import lombok.Getter;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "comments")
+@Table(name = "comments", indexes = {
+        @Index(name = "idx_comments_post_id", columnList = "post_id"),
+        @Index(name = "idx_comments_author_id", columnList = "author_id")
+})
 @Getter
 public class CommentJpaEntity {
 
